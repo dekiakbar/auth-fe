18050a1fe60f1e5920a3e2c95d1a644a79bd1268
Deki <itsdekiakbar@gmail.com>
2023-04-05 20:15:25 +0700

    lint, prettier, custom npn run lint

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,59 +2,59 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 
-export const authOptions:AuthOptions = {
-    providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-            clientSecret: process.env.GOOGLE_SECRET ?? ''
-        })
-    ],
-    pages:{
-        signIn: '/auth/signIn'
-    },
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            const url = process.env.API_URL + "/auth/signIn";
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    user: user,
-                    account: account,
-                    profile: profile,
-                    email: email,
-                    credentials: credentials
-                }),
-            });
-            
-            const body = await response.json();
-
-            if (account && response.ok) {
-                const mergedUser = {...user, ...body};
-                account.access_token = body.accessToken;
-                account.roles = body.roles;
-
-                return mergedUser;
-            }
-            
-            return false;
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signIn",
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const url = process.env.API_URL + "/auth/signIn";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        async jwt({ token, account }) {
-            if (account) {
-              token.access_token = account.access_token
-              token.roles = account.roles
-            }
-            return token
-        },
-        async session({ session, token }) {
-            session.access_token = token.access_token
-            session.roles = token.roles
-            
-            return session
-        }
-    },
-}
+        body: JSON.stringify({
+          user: user,
+          account: account,
+          profile: profile,
+          email: email,
+          credentials: credentials,
+        }),
+      });
 
-export default NextAuth(authOptions)
+      const body = await response.json();
+
+      if (account && response.ok) {
+        const mergedUser = { ...user, ...body };
+        account.access_token = body.accessToken;
+        account.roles = body.roles;
+
+        return mergedUser;
+      }
+
+      return false;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.access_token = account.access_token;
+        token.roles = account.roles;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.access_token = token.access_token;
+      session.roles = token.roles;
+
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,29 +2,28 @@ import { JWT } from "next-auth/jwt";
 import { Session } from "next-auth";
 
 declare module "next-auth/jwt" {
-    interface JWT {
-        roles?: RoleEnum[];
-        access_token?: string | null;
-    }
+  interface JWT {
+    roles?: RoleEnum[];
+    access_token?: string | null;
+  }
 }
 
 declare module "next-auth" {
-    
-    export interface HeaderProps {
-        session: Session | null;
-    }
-    
-    interface Session{
-        roles?: RoleEnum[];
-    }
+  export interface HeaderProps {
+    session: Session | null;
+  }
 
-    interface Account {
-        roles?: RoleEnum[];
-        access_token?: string | null;
-    }
+  interface Session {
+    roles?: RoleEnum[];
+  }
 
-    interface Session {
-        roles?: RoleEnum[];
-        access_token?: string | null;
-    }
+  interface Account {
+    roles?: RoleEnum[];
+    access_token?: string | null;
+  }
+
+  interface Session {
+    roles?: RoleEnum[];
+    access_token?: string | null;
+  }
 }
